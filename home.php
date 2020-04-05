661ee7678a6c7662360d5f672c34c67a9582b504
D-Gr3at <billeibinabo@gmail.com>
2020-04-05 20:01:33 +0100

    home page update

--- home.php
+++ home.php
@@ -7,83 +7,67 @@ error_reporting(E_ALL);
 include("/lib/dbfunctions.php");
 $cough_query = "SELECT cough FROM user_data WHERE cough='YES'";
 $cough_resource = pg_query($cough_query);
-$cough_cases_result = pg_fetch_all($cough_resource);
-$cough_cases = count($cough_cases_result);
+$cough_cases = pg_num_rows($cough_resource);
 
 $cold_query = "SELECT cold FROM user_data WHERE cold='YES'";
 $cold_resource = pg_query($cold_query);
-$cold_cases_result = pg_fetch_all($cold_resource);
-$cold_cases = count($cold_cases_result);
+$cold_cases = pg_num_rows($cold_resource);
 
 $diarrhea_query = "SELECT cough FROM user_data WHERE cough='YES'";
 $diarrhea_resource = pg_query($diarrhea_query);
-$diarrhea_cases_result = pg_fetch_all($diarrhea_resource);
-$diarrhea_cases = count($diarrhea_cases_result);
+$diarrhea_cases = pg_num_rows($diarrhea_resource);
 
 $sore_throat_query = "SELECT sore_throat FROM user_data WHERE sore_throat='YES'";
 $sore_throat_resource = pg_query($sore_throat_query);
-$sore_throat_cases_result = pg_fetch_all($sore_throat_resource);
-$sore_throat_cases = count($sore_throat_cases_result);
+$sore_throat_cases = pg_num_rows($sore_throat_resource);
 
 $body_ache_query = "SELECT body_aches FROM user_data WHERE body_aches='YES'";
 $body_ache_resource = pg_query($body_ache_query);
-$body_ache_cases_result = pg_fetch_all($body_ache_resource);
-$body_ache_cases = count($body_ache_cases_result);
+$body_ache_cases = pg_num_rows($body_ache_resource);
 
 $headache_query = "SELECT headache FROM user_data WHERE headache='YES'";
 $headache_resource = pg_query($headache_query);
-$headache_cases_result = pg_fetch_all($headache_resource);
-$headache_cases = count($headache_cases_result);
+$headache_cases = pg_num_rows($headache_resource);
 
 $temperature_query = "SELECT temperature FROM user_data WHERE temperature='YES'";
 $temperature_resource = pg_query($temperature_query);
-$temperature_cases_result = pg_fetch_all($temperature_resource);
-$temperature_cases = count($temperature_cases_result);
+$temperature_cases = pg_num_rows($temperature_resource);
 
 $breathing_query = "SELECT breathing FROM user_data WHERE breathing='YES'";
 $breathing_resource = pg_query($breathing_query);
-$breathing_cases_result = pg_fetch_all($breathing_resource);
-$breathing_cases = count($breathing_cases_result);
+$breathing_cases = pg_num_rows($breathing_resource);
 
 $fatigue_query = "SELECT fatigue FROM user_data WHERE fatigue='YES'";
 $fatigue_resource = pg_query($fatigue_query);
-$fatigue_cases_result = pg_fetch_all($fatigue_resource);
-$fatigue_cases = count($fatigue_cases_result);
+$fatigue_cases = pg_num_rows($fatigue_resource);
 
 $traveled_query = "SELECT traveled FROM user_data WHERE traveled='YES'";
 $traveled_resource = pg_query($traveled_query);
-$traveled_cases_result = pg_fetch_all($traveled_resource);
-$traveled_cases = count($traveled_cases_result);
+$traveled_cases = pg_num_rows($traveled_resource);
 
 $traveled_area_query = "SELECT traveled_area FROM user_data WHERE traveled_area='YES'";
 $traveled_area_resource = pg_query($traveled_area_query);
-$traveled_area_cases_result = pg_fetch_all($traveled_area_resource);
-$traveled_area_cases = count($traveled_area_cases_result);
+$traveled_area_cases = pg_num_rows($traveled_area_resource);
 
 $contact_query = "SELECT contact FROM user_data WHERE contact='YES'";
 $contact_resource = pg_query($contact_query);
-$contact_cases_result = pg_fetch_all($contact_resource);
-$contact_cases = count($contact_cases_result);
+$contact_cases = pg_num_rows($contact_resource);
 
 $low_query = "SELECT result FROM user_data WHERE result='LOW'";
 $low_resource = pg_query($low_query);
-$low_cases_result = pg_fetch_all($low_resource);
-$low_cases = count($low_cases_result);
+$low_cases = pg_num_rows($low_resource);
 
 $medium_query = "SELECT result FROM user_data WHERE result='MEDIUM'";
 $medium_resource = pg_query($medium_query);
-$medium_cases_result = pg_fetch_all($medium_resource);
-$medium_cases = count($medium_cases_result);
+$medium_cases = pg_num_rows($medium_resource);
 
 $high_query = "SELECT result FROM user_data WHERE result='HIGH'";
 $high_resource = pg_query($high_query);
-$high_cases_result = pg_fetch_all($high_resource);
-$high_cases = count($high_cases_result);
+$high_cases = pg_num_rows($high_resource);
 
 $severe_query = "SELECT result FROM user_data WHERE result='SEVERE'";
 $severe_resource = pg_query($severe_query);
-$severe_cases_result = pg_fetch_all($severe_resource);
-$severe_cases = count($severe_cases_result);
+$severe_cases = pg_num_rows($severe_resource);
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
