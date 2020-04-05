3b94c88251a989d641d2e5bd57ea83092de992c8
D-Gr3at <billeibinabo@gmail.com>
2020-04-05 20:27:55 +0100

    general update

--- lib/dbConnection.inc.php
+++ lib/dbConnection.inc.php
@@ -1,3 +1,3 @@
 <?php
-    $db_connection = pg_connect("host=localhost dbname=covid19 user=postgres password=postgres") or die("unable to connect");
+    $db_connection = pg_connect("host=ec2-3-91-112-166.compute-1.amazonaws.com dbname=db95jin19o6eq0 user=nfozdmatncibnt password=3d9a2fb7a2833edd1c072b5ea14c1a906ae614ca598134ee04d374260caefeac") or die("unable to connect");
 ?>
